07deb20a2ef87ad9c48df44e95679c63d53e2f9c
youngjaeLee8061 <young3966@gmail.com>
2020-06-12 01:13:04 +0900

    강의 추가시 강의 시간이 겹치는지 확인하는 Lecture클래스 생성 및 colorGreen 색상 생성

---
+++ app/src/main/java/com/example/classassistantproject/Lecture.java
@@ -0,0 +1,336 @@
+package com.example.classassistantproject;
+
+import android.content.Context;
+import android.widget.TextView;
+
+public class Lecture {
+    //강의를 등록했을때 강의시간이 겹치는 것을 확인
+
+    private String mon[] = new String[10];
+    private String tue[] = new String[10];
+    private String wed[] = new String[10];
+    private String thu[] = new String[10];
+    private String fri[] = new String[10];
+
+    public Lecture(){
+        for(int i=0; i<10; i++){
+            mon[i] = "";
+            tue[i] = "";
+            wed[i] = "";
+            thu[i] = "";
+            fri[i] = "";
+        }
+    }
+    public void addlecture(String lectureChoice){
+        //스케줄정보를담는 텍스트가 있을때 강의정보가 들어가는 배열에 넣기
+        //수:[1][2] 금:[1][2] 이렇게 데이터가 있다고하면 lectureChoice를 통해  이 함수에 들어와서 파싱됨
+        int tmp;
+        if((tmp = lectureChoice.indexOf("월")) > -1){
+            //tmp에 월이라는 단어가 있으면 그 위치를 반환하여 tmp에 들어가기
+            tmp+=2;
+            int frontPoint = tmp;
+            int backPoint = tmp;
+            for(int i =tmp; i < lectureChoice.length() && lectureChoice.charAt(i) != ':'; i++){
+                //이함수로 들어온 문자열의 길이와 그 문자열의 현재위치가 :가 아닐때까지 반복
+                //숫자데이터 파싱
+                if(lectureChoice.charAt(i) == '['){
+                    frontPoint = i;
+                }
+                if(lectureChoice.charAt(i) == ']'){
+                    backPoint = i;
+                    mon[Integer.parseInt(lectureChoice.substring(frontPoint +1, backPoint))] = "수업";   //넣을 데이터의 교시가 들어감.
+                }
+            }
+        }
+        if((tmp = lectureChoice.indexOf("화")) > -1){
+            //tmp에 월이라는 단어가 있으면 그 위치를 반환하여 tmp에 들어가기
+            tmp+=2;
+            int frontPoint = tmp;
+            int backPoint = tmp;
+            for(int i =tmp; i < lectureChoice.length() && lectureChoice.charAt(i) != ':'; i++){
+                //숫자데이터 파싱
+                if(lectureChoice.charAt(i) == '['){
+                    frontPoint = i;
+                }
+                if(lectureChoice.charAt(i) == ']'){
+                    backPoint = i;
+                    tue[Integer.parseInt(lectureChoice.substring(frontPoint +1, backPoint))] = "수업";   //넣을 데이터의 교시가 들어감.
+                }
+            }
+        }
+        if((tmp = lectureChoice.indexOf("수")) > -1){
+            //tmp에 월이라는 단어가 있으면 그 위치를 반환하여 tmp에 들어가기
+            tmp+=2;
+            int frontPoint = tmp;
+            int backPoint = tmp;
+            for(int i =tmp; i < lectureChoice.length() && lectureChoice.charAt(i) != ':'; i++){
+                //숫자데이터 파싱
+                if(lectureChoice.charAt(i) == '['){
+                    frontPoint = i;
+                }
+                if(lectureChoice.charAt(i) == ']'){
+                    backPoint = i;
+                    wed[Integer.parseInt(lectureChoice.substring(frontPoint +1, backPoint))] = "수업";   //넣을 데이터의 교시가 들어감.
+                }
+            }
+        }
+        if((tmp = lectureChoice.indexOf("목")) > -1){
+            //tmp에 월이라는 단어가 있으면 그 위치를 반환하여 tmp에 들어가기
+            tmp+=2;
+            int frontPoint = tmp;
+            int backPoint = tmp;
+            for(int i =tmp; i < lectureChoice.length() && lectureChoice.charAt(i) != ':'; i++){
+                //숫자데이터 파싱
+                if(lectureChoice.charAt(i) == '['){
+                    frontPoint = i;
+                }
+                if(lectureChoice.charAt(i) == ']'){
+                    backPoint = i;
+                    thu[Integer.parseInt(lectureChoice.substring(frontPoint +1, backPoint))] = "수업";   //넣을 데이터의 교시가 들어감.
+                }
+            }
+        }
+        if((tmp = lectureChoice.indexOf("금")) > -1){
+            //tmp에 월이라는 단어가 있으면 그 위치를 반환하여 tmp에 들어가기
+            tmp+=2;
+            int frontPoint = tmp;
+            int backPoint = tmp;
+            for(int i =tmp; i < lectureChoice.length() && lectureChoice.charAt(i) != ':'; i++){
+                //숫자데이터 파싱
+                if(lectureChoice.charAt(i) == '['){
+                    frontPoint = i;
+                }
+                if(lectureChoice.charAt(i) == ']'){
+                    backPoint = i;
+                    fri[Integer.parseInt(lectureChoice.substring(frontPoint +1, backPoint))] = "수업";   //넣을 데이터의 교시가 들어감.
+                }
+            }
+        }
+    }   //addlecture()
+
+    public void addlecture(String lectureChoice, String courseTitle, String courseProfessor){
+        String professor;
+        if(courseProfessor.equals("")) {
+            professor = "";
+        }else{
+            professor="("+courseProfessor+")";
+        }
+        //스케줄정보를담는 텍스트가 있을때 강의정보가 들어가는 배열에 넣기
+        //수:[1][2] 금:[1][2] 이렇게 데이터가 있다고하면 lectureChoice를 통해  이 함수에 들어와서 파싱됨
+        int tmp;
+        if((tmp = lectureChoice.indexOf("월")) > -1){
+            //tmp에 월이라는 단어가 있으면 그 위치를 반환하여 tmp에 들어가기
+            tmp+=2;
+            int frontPoint = tmp;
+            int backPoint = tmp;
+            for(int i =tmp; i < lectureChoice.length() && lectureChoice.charAt(i) != ':'; i++){
+                //이함수로 들어온 문자열의 길이와 그 문자열의 현재위치가 :가 아닐때까지 반복
+                //숫자데이터 파싱
+                if(lectureChoice.charAt(i) == '['){
+                    frontPoint = i;
+                }
+                if(lectureChoice.charAt(i) == ']'){
+                    backPoint = i;
+                    mon[Integer.parseInt(lectureChoice.substring(frontPoint +1, backPoint))] = courseTitle + professor;   //넣을 데이터의 교시가 들어감.
+                }
+            }
+        }
+        if((tmp = lectureChoice.indexOf("화")) > -1){
+            //tmp에 월이라는 단어가 있으면 그 위치를 반환하여 tmp에 들어가기
+            tmp+=2;
+            int frontPoint = tmp;
+            int backPoint = tmp;
+            for(int i =tmp; i < lectureChoice.length() && lectureChoice.charAt(i) != ':'; i++){
+                //숫자데이터 파싱
+                if(lectureChoice.charAt(i) == '['){
+                    frontPoint = i;
+                }
+                if(lectureChoice.charAt(i) == ']'){
+                    backPoint = i;
+                    tue[Integer.parseInt(lectureChoice.substring(frontPoint +1, backPoint))] = courseTitle + professor;   //넣을 데이터의 교시가 들어감.
+                }
+            }
+        }
+        if((tmp = lectureChoice.indexOf("수")) > -1){
+            //tmp에 월이라는 단어가 있으면 그 위치를 반환하여 tmp에 들어가기
+            tmp+=2;
+            int frontPoint = tmp;
+            int backPoint = tmp;
+            for(int i =tmp; i < lectureChoice.length() && lectureChoice.charAt(i) != ':'; i++){
+                //숫자데이터 파싱
+                if(lectureChoice.charAt(i) == '['){
+                    frontPoint = i;
+                }
+                if(lectureChoice.charAt(i) == ']'){
+                    backPoint = i;
+                    wed[Integer.parseInt(lectureChoice.substring(frontPoint +1, backPoint))] = courseTitle + professor;   //넣을 데이터의 교시가 들어감.
+                }
+            }
+        }
+        if((tmp = lectureChoice.indexOf("목")) > -1){
+            //tmp에 월이라는 단어가 있으면 그 위치를 반환하여 tmp에 들어가기
+            tmp+=2;
+            int frontPoint = tmp;
+            int backPoint = tmp;
+            for(int i =tmp; i < lectureChoice.length() && lectureChoice.charAt(i) != ':'; i++){
+                //숫자데이터 파싱
+                if(lectureChoice.charAt(i) == '['){
+                    frontPoint = i;
+                }
+                if(lectureChoice.charAt(i) == ']'){
+                    backPoint = i;
+                    thu[Integer.parseInt(lectureChoice.substring(frontPoint +1, backPoint))] = courseTitle + professor;   //넣을 데이터의 교시가 들어감.
+                }
+            }
+        }
+        if((tmp = lectureChoice.indexOf("금")) > -1){
+            //tmp에 월이라는 단어가 있으면 그 위치를 반환하여 tmp에 들어가기
+            tmp+=2;
+            int frontPoint = tmp;
+            int backPoint = tmp;
+            for(int i =tmp; i < lectureChoice.length() && lectureChoice.charAt(i) != ':'; i++){
+                //숫자데이터 파싱
+                if(lectureChoice.charAt(i) == '['){
+                    frontPoint = i;
+                }
+                if(lectureChoice.charAt(i) == ']'){
+                    backPoint = i;
+                    fri[Integer.parseInt(lectureChoice.substring(frontPoint +1, backPoint))] = courseTitle + professor;   //넣을 데이터의 교시가 들어감.
+                }
+            }
+        }
+    }   //addlecture()
+
+    public void setting(TextView[] mon, TextView[] tue, TextView[] wed, TextView[] thu, TextView[] fri, Context context){
+        for(int i=0; i<10; i++){
+            if(!this.mon[i].equals("")){
+                //현재시간에 미미 강의가 들어있다면
+                mon[i].setText(this.mon[i]);
+                mon[i].setBackgroundColor(context.getResources().getColor(R.color.colorGreen));
+            }
+
+            if(!this.tue[i].equals("")){
+                tue[i].setText(this.tue[i]);
+                tue[i].setBackgroundColor(context.getResources().getColor(R.color.colorGreen));
+            }
+
+            if(!this.wed[i].equals("")){
+                wed[i].setText(this.wed[i]);
+                wed[i].setBackgroundColor(context.getResources().getColor(R.color.colorGreen));
+            }
+
+            if(!this.thu[i].equals("")){
+                thu[i].setText(this.thu[i]);
+                thu[i].setBackgroundColor(context.getResources().getColor(R.color.colorGreen));
+            }
+
+            if(!this.fri[i].equals("")){
+                fri[i].setText(this.fri[i]);
+                fri[i].setBackgroundColor(context.getResources().getColor(R.color.colorGreen));
+            }
+        }
+    }
+    public boolean validate(String lectureChoice){
+        //새롭게 수강신청하려는게 기존의 신청한것과 중복되는지
+        if(lectureChoice.equals("")){    //기존에 신청한게 없을때
+            return true;
+        }
+        int tmp;
+        if((tmp = lectureChoice.indexOf("월")) > -1){
+            //tmp에 월이라는 단어가 있으면 그 위치를 반환하여 tmp에 들어가기
+            tmp+=2;
+            int frontPoint = tmp;
+            int backPoint = tmp;
+            for(int i =tmp; i < lectureChoice.length() && lectureChoice.charAt(i) != ':'; i++){
+                //숫자데이터 파싱
+                if(lectureChoice.charAt(i) == '['){
+                    frontPoint = i;
+                }
+                if(lectureChoice.charAt(i) == ']'){
+                    backPoint = i;
+                    if(!mon[Integer.parseInt(lectureChoice.substring(frontPoint +1, backPoint))].equals("")){
+                        //현재값이 공백이 아니라면(해당 시간에 데이터가 들어있다면)
+                        return false;
+                    }
+                }
+            }
+        }
+        if((tmp = lectureChoice.indexOf("화")) > -1){
+            //tmp에 월이라는 단어가 있으면 그 위치를 반환하여 tmp에 들어가기
+            tmp+=2;
+            int frontPoint = tmp;
+            int backPoint = tmp;
+            for(int i =tmp; i < lectureChoice.length() && lectureChoice.charAt(i) != ':'; i++){
+                //숫자데이터 파싱
+                if(lectureChoice.charAt(i) == '['){
+                    frontPoint = i;
+                }
+                if(lectureChoice.charAt(i) == ']'){
+                    backPoint = i;
+                    if(!tue[Integer.parseInt(lectureChoice.substring(frontPoint +1, backPoint))].equals("")){
+                        //현재값이 공백이 아니라면(해당 시간에 데이터가 들어있다면)
+                        return false;
+                    }
+                }
+            }
+        }
+        if((tmp = lectureChoice.indexOf("수")) > -1){
+            //tmp에 월이라는 단어가 있으면 그 위치를 반환하여 tmp에 들어가기
+            tmp+=2;
+            int frontPoint = tmp;
+            int backPoint = tmp;
+            for(int i =tmp; i < lectureChoice.length() && lectureChoice.charAt(i) != ':'; i++){
+                //숫자데이터 파싱
+                if(lectureChoice.charAt(i) == '['){
+                    frontPoint = i;
+                }
+                if(lectureChoice.charAt(i) == ']'){
+                    backPoint = i;
+                    if(!wed[Integer.parseInt(lectureChoice.substring(frontPoint +1, backPoint))].equals("")){
+                        //현재값이 공백이 아니라면(해당 시간에 데이터가 들어있다면)
+                        return false;
+                    }
+                }
+            }
+        }
+        if((tmp = lectureChoice.indexOf("목")) > -1){
+            //tmp에 월이라는 단어가 있으면 그 위치를 반환하여 tmp에 들어가기
+            tmp+=2;
+            int frontPoint = tmp;
+            int backPoint = tmp;
+            for(int i =tmp; i < lectureChoice.length() && lectureChoice.charAt(i) != ':'; i++){
+                //숫자데이터 파싱
+                if(lectureChoice.charAt(i) == '['){
+                    frontPoint = i;
+                }
+                if(lectureChoice.charAt(i) == ']'){
+                    backPoint = i;
+                    if(!thu[Integer.parseInt(lectureChoice.substring(frontPoint +1, backPoint))].equals("")){
+                        //현재값이 공백이 아니라면(해당 시간에 데이터가 들어있다면)
+                        return false;
+                    }
+                }
+            }
+        }
+        if((tmp = lectureChoice.indexOf("금")) > -1){
+            //tmp에 월이라는 단어가 있으면 그 위치를 반환하여 tmp에 들어가기
+            tmp+=2;
+            int frontPoint = tmp;
+            int backPoint = tmp;
+            for(int i =tmp; i < lectureChoice.length() && lectureChoice.charAt(i) != ':'; i++){
+                //숫자데이터 파싱
+                if(lectureChoice.charAt(i) == '['){
+                    frontPoint = i;
+                }
+                if(lectureChoice.charAt(i) == ']'){
+                    backPoint = i;
+                    if(!fri[Integer.parseInt(lectureChoice.substring(frontPoint +1, backPoint))].equals("")){
+                        //현재값이 공백이 아니라면(해당 시간에 데이터가 들어있다면)
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }   //validate()
+}
--- app/src/main/java/com/example/classassistantproject/MainActivity.java
+++ app/src/main/java/com/example/classassistantproject/MainActivity.java
@@ -1,96 +1,15 @@
 package com.example.classassistantproject;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button btn_sign_in;
-    Button btn_sign_up;
-
-    EditText ed_email;
-    EditText ed_pw;
-
-
-    // 파이어베이스 인스턴스
-    private FirebaseAuth mAuth;
-
-    private FirebaseDatabase db;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn_sign_in = (Button) findViewById(R.id.btn_sign_in);
-        btn_sign_up = (Button) findViewById(R.id.btn_sign_up);
-        ed_email = (EditText) findViewById(R.id.ed_email);
-        ed_pw = (EditText) findViewById(R.id.ed_pw);
-
-        mAuth = FirebaseAuth.getInstance();
-        db = FirebaseDatabase.getInstance();
-
-
-        // 로그인 버튼 클릭
-        btn_sign_in.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                mAuth.signInWithEmailAndPassword(ed_email.getText().toString(), ed_pw.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            Toast.makeText(MainActivity.this, "로그인 성공", Toast.LENGTH_SHORT).show();
-                            db.getReference("User").child(ed_email.getText().toString().replace('.', ' ')).addListenerForSingleValueEvent(new ValueEventListener() {
-                                @Override
-                                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
-                                    // dataSnapshot.chile(키).getValue 로 학과, 학번, 이름 등 가져옴
-                                    Log.d(TAG, "onDataChange: " + dataSnapshot.child("num").getValue());
-
-                                }
-
-                                @Override
-                                public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                                }
-                            });
-
-
-                        } else
-                            Toast.makeText(MainActivity.this, "로그인 실패", Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
-        });
-
-        // 회원가입 버튼 클릭
-        btn_sign_up.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
-                startActivity(intent);
-            }
-        });
-
+        //commit and push test code
     }
-
-    private static final String TAG = "MainActivity";
 }
