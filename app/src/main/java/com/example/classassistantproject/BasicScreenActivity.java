67079ac43befe4e3295f71a79f5f93c54af44a6a
youngjaeLee8061 <young3966@gmail.com>
2020-06-16 22:08:15 +0900

    로그인 성공시 메인화면으로 넘어가는 기능 구현 및 기본화면인 activity_basic_screen.xml(구현됨)와 BasicScreenActivity(생성만) 추가

---
+++ app/src/main/java/com/example/classassistantproject/BasicScreenActivity.java
@@ -0,0 +1,14 @@
+package com.example.classassistantproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BasicScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basic_screen);
+    }
+}
