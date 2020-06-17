f6a717e2a51c3a341d325174db787d7de8d980fc
suhyeokyoon <dbstngur88@naver.com>
2020-06-17 18:28:56 +0900

    FireStore Data 받아와 변수에 넣는 기능 추가&통계 기능 누를 경우 경쟁률 기능으로 이동하도록 변경

--- app/src/main/java/com/example/classassistantproject/BasicScreenActivity.java
+++ app/src/main/java/com/example/classassistantproject/BasicScreenActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -16,6 +17,8 @@ public class BasicScreenActivity extends AppCompatActivity {
         setContentView(R.layout.activity_basic_screen);
 
         findViewById(R.id.logoutBtn).setOnClickListener(onClickListener);
+        findViewById(R.id.statisticsButton).setOnClickListener(onClickListener);
+
 
         if(FirebaseAuth.getInstance().getCurrentUser() == null){
             //현재유저가 null이면 - 로그인이 안됐을때.
@@ -27,12 +30,19 @@ public class BasicScreenActivity extends AppCompatActivity {
         @Override
         public void onClick(View v) {
             switch (v.getId()) {
+                case R.id.statisticsButton:
+                    //통계버튼
+                    //경쟁률 기능의 과목 검색기능으로 넘어가기
+                    startToast("경쟁률 기능으로 이동합니다.");
+                    startstatisticsActivity();
+                    break;
                 case R.id.logoutBtn:
                     //로그아웃
                     FirebaseAuth.getInstance().signOut();
                     //로그인Activity로 돌아가기
                     startMainActivity();
                     break;
+
             }
         }
     };
@@ -40,6 +50,14 @@ public class BasicScreenActivity extends AppCompatActivity {
     private void startMainActivity(){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    private void startstatisticsActivity(){
+        Intent intent = new Intent(this, CompetsubSchActivity.class);
+        startActivity(intent);
+    }
+    private void startToast(String msg){
+        Toast.makeText(this,msg,Toast.LENGTH_SHORT).show();
     }
 
 }
