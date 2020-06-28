cea77e791cb816f3dbc1f5cc7c2913b50da19838
DongHwanYoon <dbsehdghks45@naver.com>
2020-06-28 16:02:09 +0900

    리사이클뷰 출력까지 완료2
    전체 강의 리스트만 출력되고 강의검색기능은 미구현
    푸시되지 않은 클래스들 재푸시

---
+++ app/src/main/java/com/example/classassistantproject/ClassActivity.java
@@ -0,0 +1,101 @@
+package com.example.classassistantproject;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+
+public class ClassActivity extends AppCompatActivity {
+    private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore rootRef = FirebaseFirestore.getInstance();
+    private CollectionReference courseRef = rootRef.collection("courseList");
+    //private FirebaseAuth.AuthStateListener.authStateListener;
+
+    private FirestoreRecyclerAdapter<Course, CourseViewHolder> firestoreRecyclerAdapter;
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course);
+
+        final RecyclerView recyclerView = findViewById(R.id.recycler_View);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        final TextView emptyView = findViewById(R.id.empty_view);
+        final ProgressBar progressBar = findViewById(R.id.progress_bar);
+
+        Query query = courseRef.orderBy("courseTitle", Query.Direction.ASCENDING);
+
+        FirestoreRecyclerOptions<Course> firestoreRecyclerOptions = new FirestoreRecyclerOptions.Builder<Course>()
+                .setQuery(query, Course.class)
+                .build();
+
+        firestoreRecyclerAdapter =
+                new FirestoreRecyclerAdapter<Course, CourseViewHolder>(firestoreRecyclerOptions) {
+                    @Override
+                    protected void onBindViewHolder(@NonNull CourseViewHolder holder, int position, @NonNull Course model) {
+                        holder.setCourseList(getApplicationContext(), model);
+                    }
+
+                    @NonNull
+                    @Override
+                    public CourseViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.course, parent, false);
+                        return new CourseViewHolder(view);
+                    }
+
+                    @Override
+                    public void onDataChanged() {
+                        if (progressBar != null) {
+                            progressBar.setVisibility(View.GONE);
+                        }
+
+                        if (getItemCount() == 0) {
+                            recyclerView.setVisibility(View.GONE);
+                        } else {
+                            recyclerView.setVisibility(View.VISIBLE);
+                            emptyView.setVisibility(View.GONE);
+                        }
+                    }
+
+                    @Override
+                    public int getItemCount() {
+                        return super.getItemCount();
+                    }
+                };
+
+        recyclerView.setAdapter(firestoreRecyclerAdapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firestoreRecyclerAdapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (firestoreRecyclerAdapter != null) {
+            firestoreRecyclerAdapter.stopListening();
+        }
+
+    }
+}
