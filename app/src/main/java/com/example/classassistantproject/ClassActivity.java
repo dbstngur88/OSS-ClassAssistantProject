6c611c78631554dd060b59cc70cfb4ed72d0f075
suhyeokyoon <dbstngur88@naver.com>
2020-06-30 22:25:33 +0900

    강의목록 Firebase추가 기능 중 무한 로딩 오류 수정

--- app/src/main/java/com/example/classassistantproject/ClassActivity.java
+++ app/src/main/java/com/example/classassistantproject/ClassActivity.java
@@ -2,14 +2,10 @@ package com.example.classassistantproject;
 
 import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
-import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -24,10 +20,8 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.EmailAuthCredential;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GetTokenResult;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -218,10 +212,11 @@ public class ClassActivity extends AppCompatActivity {
     //파이어스토어에 강의정보를 올려주는 메소드
     protected void uploadData(final String time, final String title, String professor, String room) {
 
+        startToast(title +"(을)를 추가합니다.");
         //set title of progress bar
-        pd.setTitle("등록중...");
+        //pd.setTitle("등록중...");
         //show progress bar when user clike save button
-        pd.show();
+        //pd.show();
 
         final Map<String, Object> putMap = new HashMap<>();
         putMap.put("courseTime", time);
@@ -269,7 +264,7 @@ public class ClassActivity extends AppCompatActivity {
                     }
                 }
             }
-//    private void startToast(String msg){
-//        Toast.makeText(this,msg,Toast.LENGTH_SHORT).show();
-//    }
+    private void startToast(String msg){
+        Toast.makeText(this,msg,Toast.LENGTH_SHORT).show();
+    }
 }
